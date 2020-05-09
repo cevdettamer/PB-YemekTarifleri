3aecea1fe09af4030f304f0cbdf094b66bf12c3a
Tamer Guven <ctamerguven@gmail.com>
2020-05-09 15:17:01 +0300

    first

---
+++ sqlsinif.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace YemekTarifleri
+{
+    public class sqlsinif
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=TAMER;Initial Catalog=PBYemekTarifleri;Integrated Security=True;Pooling=False");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
